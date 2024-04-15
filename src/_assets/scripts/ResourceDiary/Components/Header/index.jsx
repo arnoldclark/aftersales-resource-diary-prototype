0103ac915aa8cdf110f3a469e237825ce27a74e5
Jamie Cassidy <jamiec90@gmail.com>
2024-04-15 11:32:19 +0100

    Clean up

--- src/_assets/scripts/ResourceDiary/Components/Header/index.jsx
+++ src/_assets/scripts/ResourceDiary/Components/Header/index.jsx
@@ -4,25 +4,6 @@ import { Link } from 'react-router-dom';
 
 const Header = ({active}) => {
   return (
-    // <header class="ch-navbar">
-    //   <div class="ch-container">
-    //     <div class="ch-row">
-    //       <div class="xs:ch-col--12 sm:ch-col--3">
-    //         <a href="/" class="ch-navbar__title ch-mv--3">
-    //           <img src="/images/ac-emblem.svg" alt="Arnold Clark" />
-    //           Branch resource
-    //         </a>
-    //       </div>
-    //       <div class="xs:ch-col--12 sm:ch-col--9">
-    //         <ul class="ch-navbar__nav ch-pull--left" data-id="ch-navbar-menu">
-    //           <li><a href="">Manage Resource</a></li>
-    //           <li><a href="">Resource Diary</a></li>
-    //         </ul>
-    //         <a href="#" class="ch-navbar__user ch-bg--ac-magenta ch-color--white ch-pull--right ch-mv--3">JC</a>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </header>
 
     <header class="ch-navbar">
       <div class="ch-container">
